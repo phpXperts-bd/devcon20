cf3b91a16799eea2c9c1d26da68d3bed951063f3
M A Hossain Tonu <mahtonu@gmail.com>
2020-03-07 15:53:31 +0600

    Gallery added

--- resources/views/angularbd/partials/previous-meetup-gallery.blade.php
+++ resources/views/angularbd/partials/previous-meetup-gallery.blade.php
@@ -1,49 +1,89 @@
 <div class="Meetup__gallery" id="gallery">
-    <h4 class="Meetup__sectionTitle">Previous Meetup Gallery</h4>
+    <h4 class="Meetup__sectionTitle">Previous Events Gallery</h4>
     <div class="gallery">
         <div class="gallery__contain">
             <div class="gallery__draggable">
                 <div class="gallery__container">
                     <div class="gallery__row">
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-1.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/1.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-2.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/2.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-3.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/3.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-4.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/4.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-5.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/5.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-6.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/6.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-7.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/7.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-8.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/8.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-9.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/9.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-10.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/10.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-11.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/11.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-12.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/12.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-13.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/13.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-14.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/14.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-15.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/15.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-16.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/16.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-17.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/17.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-18.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/18.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-19.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/19.jpg') }}" alt="">
                         </div>
-                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/ng-bd-20.jpg') }}" alt="">
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/20.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/21.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/22.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/23.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/24.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/25.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/26.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/27.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/28.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/29.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/30.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/31.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/32.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/33.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/34.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/35.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/36.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/37.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/38.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/39.jpg') }}" alt="">
+                        </div>
+                        <div class="gallery__col gallery__col--80"><img src="{{ asset('phpxperts/images/gallery/40.jpg') }}" alt="">
                         </div>
                     </div>
                 </div>
