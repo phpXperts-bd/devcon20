90c145da1c796fb7bf0307ab4e0241a204c2453b
M A Hossain Tonu <mahtonu@vantage.com>
2020-11-08 04:43:13 +0600

    letsencrypt with supervisor update

--- docker/app/init-letsencrypt.sh
+++ docker/app/init-letsencrypt.sh
@@ -28,20 +28,18 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p path
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+mkdir -p $path
+openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout "$path/privkey.pem" \
+    -out "$path/fullchain.pem" \
+    -subj "/CN=localhost"
 echo
 
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf
 echo
 
 
@@ -61,12 +59,11 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal
 echo
