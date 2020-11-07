1b7025c65ff8b6213b2883d4c1d837283b2d4c99
M A Hossain Tonu <mahtonu@vantage.com>
2020-11-08 02:40:59 +0600

    letsencrypt with supervisor update

--- docker/app/init-letsencrypt.sh
+++ docker/app/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(phpxperts.net www.phpxperts.net)
+domains=(phpxperts.net)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mahtonu@gmail.com" # Adding a valid address is strongly recommended
